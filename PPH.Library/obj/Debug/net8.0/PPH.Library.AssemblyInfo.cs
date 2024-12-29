799537414ae9fb06dfa410140a0553a4c5103f71
Gungnir0616 <2733156829@qq.com>
2024-12-29 08:33:40 +0800

    + Dependence

--- PPH.Library/obj/Debug/net8.0/PPH.Library.AssemblyInfo.cs
+++ PPH.Library/obj/Debug/net8.0/PPH.Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PPH.Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e041c7927ed10ce5dae9fb168ae278b325ce1ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("PPH.Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PPH.Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
